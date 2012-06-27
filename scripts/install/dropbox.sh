19230be3f9e61c9a823a4d50960ee480b6ec3b1f
Nikolai Plath <der.el.kuku@gmail.com>
2012-06-27 20:07:30 +0000

    Update scripts

--- scripts/install/dropbox.sh
+++ scripts/install/dropbox.sh
@@ -6,9 +6,12 @@
 # * "Install" it
 # * Create an autostart link
 #
+# @ 2012 by elkuku
 
-cd ~
+# Install to /home/<user>
+cd $HOME
 
+# Check architecture
 if [ `uname -m` = "x86_64" ]
 then
     # 64 bit
@@ -16,7 +19,6 @@ then
 else
     # 32 bit
     arch="x86"
-echo 32
 fi
 
 # Download and unpack
@@ -26,6 +28,7 @@ wget -O - "https://www.dropbox.com/download?plat=lnx.$arch" --no-check-certifica
 ln -s $HOME/.dropbox-dist/dropboxd $HOME/.kde4/Autostart/dropboxd
 
 # Start the daemon
-.dropbox-dist/dropboxd &
+echo; echo 'Starting the daemon...'
+.dropbox-dist/dropboxd
 
 echo "Finished =;)"
